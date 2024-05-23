b89954757c0f1861997b75a81d12cd6541054f70
gvbvdxx <gvbvdxx@outlook.com>
2024-05-23 08:25:33 -0400

    Uploading stuff

---
+++ mods/editorScripts/test.js
@@ -0,0 +1,18 @@
+/*console.log("Editor test script imported");
+
+editor.addEventListener("clearlevel", function () {
+	console.log("clearlevel event emitted.");
+});
+editor.addEventListener("tilecreate", function (a,b) {
+	console.log("tilecreate event emitted.",a,b);
+});
+editor.addEventListener("savetile", function (a,b) {
+	console.log("savetile event emitted.",a,b);
+	b.test = true;
+});
+editor.addEventListener("loadtile", function (a,b) {
+	console.log("loadtile event emitted.",a,b);
+});
+editor.addEventListener("tick", function () {
+	//console.log("tick event emitted.");
+});*/
